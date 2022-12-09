87bd626c6db840c343c2fd4defbaf0e60373f8f4
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-12-08 21:28:36 -0300

    chore(notifications): init notifications module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NotificationsModule } from './notifications/notifications.module';
 @Module({
-  imports: [],
+  imports: [NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
